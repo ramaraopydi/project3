bb0b782b4c9771cb9c4341fd5cf77f83012071a4
ramaraopydi <prrao234@gmail.com>
2015-05-18 14:53:25 +0530

    rb4 merged

--- demo/demo.js
+++ demo/demo.js
@@ -8,11 +8,8 @@
 			$('#switch-style').animate({'left':'-200px'});
 		}else{
 			$(this).addClass('open');
-<<<<<<< HEAD
-			$('#switch-style').animate({'left':'20px'});
-=======
+
 			$('#switch-style').animate({'left':'0px'});
->>>>>>> origin/rb4
 		}
 	});
 });
